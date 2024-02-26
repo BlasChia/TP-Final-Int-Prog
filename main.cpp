15f979775c0349574b5d9c83f6145fcedb57d844
BlasChia <blaschiapero13@gmail.com>
2024-02-26 18:00:02 -0300

    intento clases

--- main.cpp
+++ main.cpp
@@ -1,7 +1,64 @@
 #include<iostream>
 using namespace std;
 
+class Juego {
+public:
+	Juego(int, int, int, int);
+	~Juego();
+	int obtNumVidas();
+	int obtTiempo();
+	bool ubicarInicio(int);
+	int obtPunt();
+	int obtFilasAvanzadas();
+	void numVidas(int);
+	void tiempo(int);
+	void lugarInicio(int, bool);
+	void puntaje(int);
+	void filasAvanzadas(int);
+	
+private:
+	int vidas;
+	int puntos;
+	int filasAvanz;
+	int tiempoRest;
+	bool inicio[5];
+};
+
+class Rana {
+public:
+	Rana(double, double, int, int, double, double, char);
+	~Rana();
+	double obtCentroX();
+	double obtCentroY();
+	int obtLargo();
+	int obtCuadriculaY();
+	double obtDistIzq();
+	double obtDistDer();
+	char obtOrient();
+	void estCentroX(double);
+	void estCentroY(double);
+	void estLargo(int);
+	void estCuadriculaY(int);
+	void estDistIzq(double);
+	void estDistDer(double);
+	void estOrient(char);
+	void dibujarRana();
+	void moverRana(char, Juego&);
+	
+private:
+	double centroX;
+	double centroY;
+	int largo;
+	int cuadriculaY;
+	double distIzq;
+	double distDer;
+	char orientacion;
+};
+
+
 int main (int argc, char *argv[]) {
+	
+	
 	
 	return 0;
 }
