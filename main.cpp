b928c0b7bfc2728f1bd502bcd82bb85c700c8056
BlasChia <blaschiapero13@gmail.com>
2024-02-26 20:16:22 -0300

    avances

--- main.cpp
+++ main.cpp
@@ -1,65 +1,76 @@
 #include<iostream>
+#include<windows.h>
+#include<time.h>
+#include<stdlib.h>
+#include<cstdlib>
+#include<conio.h>
+#include<time.h>
+
 using namespace std;
 
-class Juego {
-public:
-	Juego(int, int, int, int);
-	~Juego();
-	int obtNumVidas();
-	int obtTiempo();
-	bool ubicarInicio(int);
-	int obtPunt();
-	int obtFilasAvanzadas();
-	void numVidas(int);
-	void tiempo(int);
-	void lugarInicio(int, bool);
-	void puntaje(int);
-	void filasAvanzadas(int);
-	
-private:
-	int vidas;
-	int puntos;
-	int filasAvanz;
-	int tiempoRest;
-	bool inicio[5];
-};
+const int ANCHO=600, ALTO=200;
 
-class Rana {
-public:
-	Rana(double, double, int, int, double, double, char);
-	~Rana();
-	double obtCentroX();
-	double obtCentroY();
-	int obtLargo();
-	int obtCuadriculaY();
-	double obtDistIzq();
-	double obtDistDer();
-	char obtOrient();
-	void estCentroX(double);
-	void estCentroY(double);
-	void estLargo(int);
-	void estCuadriculaY(int);
-	void estDistIzq(double);
-	void estDistDer(double);
-	void estOrient(char);
-	void dibujarRana();
-	void moverRana(char, Juego&);
-	
-private:
-	double centroX;
-	double centroY;
-	int largo;
-	int cuadriculaY;
-	double distIzq;
-	double distDer;
-	char orientacion;
-};
+const int dAnc=10, dAlt=5;
 
-
-int main (int argc, char *argv[]) {
-	
-	
-	
-	return 0;
+void gotoxy(int x, int y) {
+	HANDLE hcon = GetStdHandle(STD_OUTPUT_HANDLE);
+	COORD dwPos;
+	dwPos.X = x;
+	dwPos.Y = y;
+	SetConsoleCursorPosition(hcon, dwPos);
 }
 
+void dibujar(char plano[ALTO/dAlt][ANCHO/dAnc+1], int A, int B, char C) {
+	
+	B = ALTO/dAlt-1-B;
+	if(A<0 || B<0 || A >= ANCHO/dAnc || B >= ALTO/dAlt) return;
+	plano[B][A] = C;
+}
+
+class Mapa{
+	
+private:
+	
+	
+	
+	
+public:
+	void pintarMapa(char plano[ALTO/dAlt][ANCHO/dAnc+1]) {
+		for(int i=0; i<ANCHO/dAnc;i++) {
+			for(int j=0;j>ALTO/dAlt;j++) {
+				if(i==0 || i==59 || j==0 || j==39)
+					dibujar(plano,i,j,'*');
+				if(j%3==0)
+					dibujar(plano,i,j,'_');
+			}
+		}
+	}
+};
+
+
+main() {
+	
+	char plano[ALTO/dAlt][ANCHO/dAnc+1];
+	for(int i=0; i<ALTO/dAlt; i++)
+		plano[i][ANCHO/dAnc] = '\n';
+	plano[ALTO/dAlt+1][ANCHO/dAnc] = '\o';
+	
+	mapa j1;
+	
+	bool juego=true;
+	
+	while(juego){
+		for(int i=0; i<ALTO/dAlt; i++){
+			for(int j=0; j<ANCHO/dAnc; j++) {
+				plano[i][j]= ' ';
+			}
+		}
+		
+		j1.pintarMapa(plano);
+		
+		gotoxy(0,0);
+		puts(plano[0]);
+	}
+	
+	return 0;
+};
