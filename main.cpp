a3eae1db9d7fd80a31d6a42bee151cba6254f9a2
BlasChia <blaschiapero13@gmail.com>
2024-02-27 17:32:51 -0300

    avances y otras pruebas

--- main.cpp
+++ main.cpp
@@ -1,76 +1,189 @@
-#include<iostream>
-#include<windows.h>
-#include<time.h>
-#include<stdlib.h>
-#include<cstdlib>
-#include<conio.h>
-#include<time.h>
+#include <iostream>
+#include <conio.h>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
 
-using namespace std;
+enum class Direction { Up, Down, Left, Right };
 
-const int ANCHO=600, ALTO=200;
-
-const int dAnc=10, dAlt=5;
-
-void gotoxy(int x, int y) {
-	HANDLE hcon = GetStdHandle(STD_OUTPUT_HANDLE);
-	COORD dwPos;
-	dwPos.X = x;
-	dwPos.Y = y;
-	SetConsoleCursorPosition(hcon, dwPos);
-}
-
-void dibujar(char plano[ALTO/dAlt][ANCHO/dAnc+1], int A, int B, char C) {
-	
-	B = ALTO/dAlt-1-B;
-	if(A<0 || B<0 || A >= ANCHO/dAnc || B >= ALTO/dAlt) return;
-	plano[B][A] = C;
-}
-
-class Mapa{
-	
-private:
-	
-	
-	
-	
+class GameObject {
 public:
-	void pintarMapa(char plano[ALTO/dAlt][ANCHO/dAnc+1]) {
-		for(int i=0; i<ANCHO/dAnc;i++) {
-			for(int j=0;j>ALTO/dAlt;j++) {
-				if(i==0 || i==59 || j==0 || j==39)
-					dibujar(plano,i,j,'*');
-				if(j%3==0)
-					dibujar(plano,i,j,'_');
+	int x, y;
+	char symbol;
+	
+	GameObject(int x, int y, char symbol) : x(x), y(y), symbol(symbol) {}
+	
+	virtual void Draw() const {
+		std::cout << symbol << " ";
+	}
+	
+	virtual bool CanMove(Direction direction, const std::vector<GameObject*>& objects) const {
+		int newX = x, newY = y;
+		switch (direction) {
+		case Direction::Up:
+			newY--;
+		break;
+		case Direction::Down:
+			newY++;
+		break;
+		case Direction::Left:
+			newX--;
+		break;
+		case Direction::Right:
+			newX++;
+		break;
+		}
+		for (const auto& obj : objects) {
+			if (obj->x == newX && obj->y == newY) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	virtual void Move(Direction direction, std::vector<GameObject*>& objects) {
+		if (CanMove(direction, objects)) {
+			switch (direction) {
+			case Direction::Up:
+				y--;
+			break;
+			case Direction::Down:
+				y++;
+			break;
+			case Direction::Left:
+				x--;
+			break;
+			case Direction::Right:
+				x++;
+			break;
 			}
 		}
 	}
 };
 
-
-main() {
+class Frog : public GameObject {
+public:
+	Frog(int x, int y) : GameObject(x, y, 'F') {}
 	
-	char plano[ALTO/dAlt][ANCHO/dAnc+1];
-	for(int i=0; i<ALTO/dAlt; i++)
-		plano[i][ANCHO/dAnc] = '\n';
-	plano[ALTO/dAlt+1][ANCHO/dAnc] = '\o';
-	
-	mapa j1;
-	
-	bool juego=true;
-	
-	while(juego){
-		for(int i=0; i<ALTO/dAlt; i++){
-			for(int j=0; j<ANCHO/dAnc; j++) {
-				plano[i][j]= ' ';
+	void Move(Direction direction, std::vector<GameObject*>& objects) override {
+		if (CanMove(direction, objects)) {
+			switch (direction) {
+			case Direction::Up:
+				y--;
+			break;
+			case Direction::Down:
+				y++;
+			break;
+			case Direction::Left:
+				x--;
+			break;
+			case Direction::Right:
+				x++;
+			break;
 			}
 		}
-		
-		j1.pintarMapa(plano);
-		
-		gotoxy(0,0);
-		puts(plano[0]);
 	}
+};
+
+class Car : public GameObject {
+public:
+	Car(int x, int y) : GameObject(x, y, 'C') {}
 	
+	void Move(Direction direction, std::vector<GameObject*>& objects) override {
+		if (CanMove(direction, objects)) {
+			switch (direction) {
+			case Direction::Up:
+				y--;
+			break;
+			case Direction::Down:
+				y++;
+			break;
+			case Direction::Left:
+				x--;
+			break;
+			case Direction::Right:
+				x++;
+			break;
+			}
+		}
+	}
+};
+
+class Log : public GameObject {
+public:
+	Log(int x, int y) : GameObject(x, y, 'L') {}
+	
+	bool CanMove(Direction direction, const std::vector<GameObject*>& objects) const override {
+		int newX = x, newY = y;
+		switch (direction) {
+		case Direction::Up:
+			newY--;
+		break;
+		case Direction::Down:
+			newY++;
+		break;
+		case Direction::Left:
+			newX--;
+		break;
+		case Direction::Right:
+			newX++;
+		break;
+		}
+		for (const auto& obj : objects) {
+			if (obj->x == newX && obj->y == newY) {
+				return false;
+			}
+		}
+		return true;
+	}
+};
+
+class GameManager {
+public:
+	void StartGame() {
+		srand(time(0));
+		std::vector<GameObject*> objects;
+		objects.push_back(new Frog(5, 5));
+		for (int i = 0; i < 10; i++) {
+			objects.push_back(new Car(rand() % 10, rand() % 10));
+		}
+		for (int i = 0; i < 5; i++) {
+			objects.push_back(new Log(rand() % 10, rand() % 10));
+		}
+		while (true) {
+			system("cls");
+			for (int i = 0; i < 10; i++) {
+				for (int j = 0; j < 10; j++) {
+					bool found = false;
+					for (const auto& obj : objects) {
+						if (obj->x == i && obj->y == j) {
+							obj->Draw();
+							found = true;
+							break;
+						}
+					}
+					if (!found) {
+						std::cout << "  ";
+					}
+				}
+				std::cout << std::endl;
+			}
+			char key = _getch();
+			if (key == 'w' || key == 'W') {
+				objects[0]->Move(Direction::Up, objects);
+			} else if (key == 's' || key == 'S') {
+				objects[0]->Move(Direction::Down, objects);
+			} else if (key == 'a' || key == 'A') {
+				objects[0]->Move(Direction::Left, objects);
+			} else if (key == 'd' || key == 'D') {
+				objects[0]->Move(Direction::Right, objects);
+			}
+		}
+	}
+};
+
+int main() {
+	GameManager manager;
+	manager.StartGame();
 	return 0;
-};
+}
